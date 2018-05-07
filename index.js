898066763ff9f3d603448790086c405149264b2a
Asaf Davidov <adavidov@conncoll.edu>
2018-05-07 23:16:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function getFirstSelector(selector){
   console.log (document.querySelector(selector).innerHTML);
-  return document.querySelector(selector).innerHTML;
+  return document.querySelector(selector);
  
 }
 

db20edba24b33c97f79f794fdf6fc68dbc5d2a6d
Asaf Davidov <adavidov@conncoll.edu>
2018-05-07 23:20:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(target){
-  return document.getElementbyID(target);
+  return document.getElementByID(target);
 }
 
 //function increaseRankBy(n){
